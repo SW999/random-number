8c8558ae8b462296774c55a96f04559f5157fd88
Siarhei Vaitehovich <swarog2002@mail.ru>
2019-04-21 19:04:28 +0300

    Test for custom debounce function

--- src/tests/services/utils.test.js
+++ src/tests/services/utils.test.js
@@ -5,29 +5,63 @@ Object.defineProperty(global.self, 'crypto', {
     getRandomValues: arr => crypto.randomBytes(arr.length),
   },
 });
-import { getRandomInt } from '../../services/utils';
+import { getRandomInt, debounceEvent } from '../../services/utils';
 
-test('Should generate random number from 0 to 100', () => {
-  const value = getRandomInt(0, 100);
+describe('Test for getRandomInt', () => {
+  test('1. Should generate random number from 0 to 100', () => {
+    const value = getRandomInt(0, 100);
 
-  expect(value).toBeGreaterThanOrEqual(0);
-  expect(value).toBeLessThanOrEqual(100);
+    expect(value).toBeGreaterThanOrEqual(0);
+    expect(value).toBeLessThanOrEqual(100);
+  });
+
+  test('2. Should throw Error for a parameter with string type', () => {
+    const error = () => getRandomInt('rr', 100);
+
+    expect(error).toThrowError(new RangeError('Min and max are expected to be numbers'));
+  });
+
+  test('3. Should throw Error for equal min and max', () => {
+    const error = () => getRandomInt(100, 100);
+
+    expect(error).toThrowError(new RangeError('Expected max to be no less than (min + 1)'));
+  });
+
+  test('4. Should throw Error for negative min value', () => {
+    const error = () => getRandomInt(-10, 100);
+
+    expect(error).toThrowError(new RangeError('Expected min to be >= 0'));
+  });
 });
 
-test('Should throw Error for a parameter with string type', () => {
-  const error = () => getRandomInt('rr', 100);
+jest.useFakeTimers();
 
-  expect(error).toThrowError(new RangeError('Min and max are expected to be numbers'));
-});
+describe('Test for debounceEvent function', () => {
+  let func;
 
-test('Should throw Error for equal min and max', () => {
-  const error = () => getRandomInt(100, 100);
+  beforeEach(() => {
+    func = jest.fn();
+  });
 
-  expect(error).toThrowError(new RangeError('Expected max to be no less than (min + 1)'));
-});
+  test('1. Should run just once', () => {
+    const debouncedFunc = debounceEvent(func, 1000);
+    debouncedFunc();
+    debouncedFunc();
+    debouncedFunc();
+    // fast-forward time
+    jest.runAllTimers();
 
-test('Should throw Error for negative min value', () => {
-  const error = () => getRandomInt(-10, 100);
+    expect(func).toBeCalledTimes(1);
+  });
 
-  expect(error).toThrowError(new RangeError('Expected min to be >= 0'));
+  test('2. Should run with default timeout', () => {
+    const debouncedFunc2 = debounceEvent(func);
+    debouncedFunc2();
+    debouncedFunc2();
+    debouncedFunc2();
+    // fast-forward time
+    jest.runAllTimers();
+
+    expect(func).toBeCalledTimes(1);
+  });
 });
