1ede2805274f1afb0f985b051d993fa47bff27ae
Siarhei Vaitehovich <swarog2002@mail.ru>
2019-05-04 16:49:12 +0300

    Add Github icon with link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import github from '../img/github.svg';
 
 export default class Footer extends React.Component {
   render() {
-    return ReactDOM.createPortal(<span>© <a href="https://github.com/SW999/random-number" rel="nofollow">SW999</a> 2019</span>, document.getElementById('footer'));
+    return ReactDOM.createPortal(<span><a href="https://github.com/SW999/random-number" rel="nofollow" title="Github page"><img src={github} alt="github" width="24" height="24"/></a> © SW999 2019</span>, document.getElementById('footer'));
   }
 }
